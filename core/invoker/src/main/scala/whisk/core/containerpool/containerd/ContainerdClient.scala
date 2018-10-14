f847ee321633826566e0048a01634210990b2db7
Konrad Ohms <konrad.ohms@de.ibm.com>
2018-10-14 15:16:03 +0200

    add comments, add isOomKilled

--- core/invoker/src/main/scala/whisk/core/containerpool/containerd/ContainerdClient.scala
+++ core/invoker/src/main/scala/whisk/core/containerpool/containerd/ContainerdClient.scala
@@ -36,28 +36,38 @@ import scala.concurrent.duration.Duration
 import scala.util.{Failure, Success}
 
 /**
-  * Configuration for docker client command timeouts.
+  * Configuration for wskc client command timeouts.
   */
 case class WskcClientTimeoutConfig(run: Duration,
                                    rm: Duration,
+                                   inspect: Duration,
                                    pull: Duration,
-                                   ps: Duration,
                                    pause: Duration,
                                    unpause: Duration,
                                    init: Duration,
                                    cleanup: Duration)
 
 /**
-  * Configuration for docker client
+  * Configuration for wskc client
   */
 case class WskcClientConfig(timeouts: WskcClientTimeoutConfig)
 
+/**
+  * Serves as interface to the wskc CLI tool which will use containerd and CNI to manage network and containers.
+  *
+  * Be cautious with the ExecutionContext passed to this, as the
+  * calls to the CLI are blocking.
+  *
+  * Only one instance per invoker is needed.
+  */
 class ContainerdClient( config: WskcClientConfig = loadConfigOrThrow[WskcClientConfig](ConfigKeys.dockerClient))(
   executionContext: ExecutionContext)(
   implicit log: Logging, as: ActorSystem)
   extends WskcApi with ProcessRunner {
   implicit private val ec = executionContext
 
+  // Determines how to run wskc. Failure to find the wskc binary implies
+  // a failure to initialize this instance of ContainerdClient.
   protected val wskcCmd: Seq[String] = {
     val alternatives = List("/usr/local/bin/wskc", "/usr/bin/wskc", "/bin/wskc")
 
@@ -70,16 +80,19 @@ class ContainerdClient( config: WskcClientConfig = loadConfigOrThrow[WskcClientC
     Seq(wskcBin, "--namespace", "wsk")
   }
 
+  // wskc --namespace wsk init
   def init(): Future[Unit] = {
     implicit val transid = TransactionId.invoker
     runCmd(Seq("init"), config.timeouts.init).map(_ => ())
   }
 
+  // wskc --namespace wsk cleanup
   def cleanup(): Future[Unit] = {
     implicit val transid = TransactionId.invoker
     runCmd(Seq("cleanup"), config.timeouts.cleanup).map(_ => ())
   }
 
+  // wskc --namespace wsk run --memory=256 docker.io/openwhisk/action-nodejs-v8:latest mycontainer
   def run(image: String, name: String, args: Seq[String] = Seq.empty[String])(
     implicit transid: TransactionId): Future[ContainerId] = {
     blocking {
@@ -93,24 +106,36 @@ class ContainerdClient( config: WskcClientConfig = loadConfigOrThrow[WskcClientC
     }
   }
 
+  // wskc --namespace wsk pause mycontainer
   def pause(id: ContainerId)(implicit transid: TransactionId): Future[Unit] =
     runCmd(Seq("pause", id.asString), config.timeouts.pause).map(_ => ())
 
+  // wskc --namespace wsk unpause mycontainer
   def unpause(id: ContainerId)(implicit transid: TransactionId): Future[Unit] =
     runCmd(Seq("unpause", id.asString), config.timeouts.unpause).map(_ => ())
 
+  // wskc --namespace wsk rm mycontainer
   def rm(id: ContainerId)(implicit transid: TransactionId): Future[Unit] =
     runCmd(Seq("rm", id.asString), config.timeouts.rm).map(_ => ())
 
+  // wskc --namespace wsk inspect --isOomKilled myContainer
+  def isOomKilled(id: ContainerId)(implicit transid: TransactionId): Future[Boolean] =
+    runCmd(Seq("inspect", "--isOomKilled", id.asString), config.timeouts.inspect).map(_.toBoolean)
+
+  // wskc --namespace wsk network del mycontainer
   def deleteNetwork(id: ContainerId)(implicit transid: TransactionId): Future[Unit] =
     runCmd(Seq("network", "del", id.asString), config.timeouts.rm).map(_ => ())
+
+  // wskc --namespace wsk network add mycontainer
+  def createNetwork(id: ContainerId)(implicit transid: TransactionId): Future[Unit] =
+    runCmd(Seq("network", "add", id.asString), config.timeouts.rm).map(_ => ())
 
   /**
     * Stores pulls that are currently being executed and collapses multiple
     * pulls into just one. After a pull is finished, the cached future is removed
     * to enable constant updates of an image without changing its tag.
     */
-  // ctr --namespace wsk-action-container images pull docker.io/openwhisk/nodejs6action:latest
+  // wskc --namespace wsk pull docker.io/openwhisk/action-nodejs-v8:latest
   private val pullsInFlight = TrieMap[String, Future[Unit]]()
   def pull(image: String)(implicit transid: TransactionId): Future[Unit] =
     pullsInFlight.getOrElseUpdate(image, {
@@ -136,12 +161,12 @@ class ContainerdClient( config: WskcClientConfig = loadConfigOrThrow[WskcClientC
 trait WskcApi {
 
   /**
-    * Initialize a containerd namespace
+    * Initialize a containerd namespace and creates shared files on disk
     */
   def init(): Future[Unit]
 
   /**
-    * Cleanup a containerd namespace
+    * Cleanup a containerd namespace (removes all containers in namespace)
     */
   def cleanup(): Future[Unit]
 
@@ -180,15 +205,20 @@ trait WskcApi {
   def rm(id: ContainerId)(implicit transid: TransactionId): Future[Unit]
 
   /**
-    * Removes a container network
+    * Removes a device pair from a network bridge
     *
     * @param id the id of the network owning container to remove
     * @return a Future completing according to the command's exit-code
     */
   def deleteNetwork(id: ContainerId)(implicit  transid: TransactionId): Future[Unit]
 
-  //def stoppableTasks()(implicit transid: TransactionId): Future[Seq[ContainerId]]
-  //def containers()(implicit transid: TransactionId): Future[Seq[ContainerId]]
+  /**
+    * Creates a device pair and connects it to a network bridge (creates bridge if necessary)
+    *
+    * @param id the id of the network owning container to remove
+    * @return a Future completing according to the command's exit-code
+    */
+  def createNetwork(id: ContainerId)(implicit  transid: TransactionId): Future[Unit]
 
   /**
     * Pulls the given image.
@@ -197,4 +227,13 @@ trait WskcApi {
     * @return a Future completing once the pull is complete
     */
   def pull(image: String)(implicit transid: TransactionId): Future[Unit]
+
+  /**
+    * Determines whether the given container was killed due to
+    * memory constraints.
+    *
+    * @param id the id of the container to check
+    * @return a Future containing whether the container was killed or not
+    */
+  def isOomKilled(id: ContainerId)(implicit transid: TransactionId): Future[Boolean]
 }
--- core/invoker/src/main/scala/whisk/core/containerpool/containerd/ContainerdContainer.scala
+++ core/invoker/src/main/scala/whisk/core/containerpool/containerd/ContainerdContainer.scala
@@ -16,22 +16,36 @@
  */
 
 package whisk.core.containerpool.containerd
+import java.time.Instant
 import akka.actor.ActorSystem
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration._
+import whisk.common.Logging
+import whisk.common.TransactionId
+import whisk.core.containerpool._
+import whisk.core.entity.ActivationResponse.{ConnectionError, MemoryExhausted}
+import whisk.core.entity.{ActivationEntityLimit, ByteSize}
+import whisk.core.entity.size._
 import akka.stream.scaladsl.Source
 import akka.util.ByteString
-import whisk.common.{Logging, TransactionId}
-import whisk.core.containerpool._
-import whisk.core.entity.ByteSize
-import whisk.core.entity.size._
-import whisk.http.Messages
+import spray.json._
 import whisk.core.entity.ExecManifest.ImageName
-
-import scala.concurrent.{ExecutionContext, Future}
+import whisk.http.Messages
 
 object ContainerdContainer {
+  /**
+    * Creates a container running on a docker daemon.
+    *
+    * @param transid transaction creating the container
+    * @param image either a user provided (Left) or OpenWhisk provided (Right) image
+    * @param memory memorylimit of the container
+    * @param cpuShares sharefactor for the container
+    * @param environment environment variables to set on the container
+    * @param name optional name for the container
+    * @return a Future which either completes with a ContainerdContainer or one of two specific failures
+    */
   def create(transid: TransactionId,
              image: Either[ImageName, String],
-             userProvidedImage: Boolean = false,
              memory: ByteSize = 256.MB,
              cpuShares: Int = 0,
              environment: Map[String, String] = Map.empty,
@@ -74,8 +88,6 @@ object ContainerdContainer {
         Future.successful(true)
     }
 
-
-
     for {
       pullSuccessful <- pulled
       cname <- name
@@ -83,12 +95,26 @@ object ContainerdContainer {
         .getOrElse(Future.failed(WhiskContainerStartupError("No name specified")))
       id <- wskc.run(imageToUse, cname, environmentArgs).recoverWith {
         case _ =>
-          Future.failed(WhiskContainerStartupError(Messages.resourceProvisionError))
+          // Iff the pull was successful, we assume that the error is not due to an image pull error, otherwise
+          // the docker run was a backup measure to try and start the container anyway. If it fails again, we assume
+          // the image could still not be pulled and wasn't available locally.
+          if (pullSuccessful) {
+            Future.failed(WhiskContainerStartupError(Messages.resourceProvisionError))
+          } else {
+            Future.failed(BlackboxStartupError(Messages.imagePullError(imageToUse)))
+          }
       }
     } yield new ContainerdContainer(id, ContainerAddress("127.0.0.1")) //TODO get real IP here
   }
 }
 
+/**
+  * Represents a container as run by containerd.
+  *
+  * @constructor
+  * @param id the id of the container
+  * @param addr the ip of the container
+  */
 class ContainerdContainer(protected val id: ContainerId, protected val addr: ContainerAddress)(
   implicit wskc: WskcApi,
   override protected val as: ActorSystem,
@@ -96,14 +122,71 @@ class ContainerdContainer(protected val id: ContainerId, protected val addr: Con
   protected val logging: Logging)
     extends Container {
 
+  protected val waitForOomState: FiniteDuration = 2.seconds
+  protected val filePollInterval: FiniteDuration = 5.milliseconds
+
   override def suspend()(implicit transid: TransactionId): Future[Unit] = wskc.pause(id)
   override def destroy()(implicit transid: TransactionId): Future[Unit] = {
     super.destroy()
     wskc.rm(id)
   }
+
   def resume()(implicit transid: TransactionId): Future[Unit] = wskc.unpause(id)
+
+  // TODO solve logging via containerd, currently it does not write into files nor pipes
   def logs(limit: ByteSize, waitForSentinel: Boolean)(implicit transid: TransactionId): Source[ByteString, Any] = {
     Source.empty[ByteString]
   }
 
+  /**
+    * Was the container killed due to memory exhaustion?
+    *
+    * @param retries number of retries to make
+    * @return a Future indicating a memory exhaustion situation
+    */
+  private def isOomKilled(retries: Int = (waitForOomState / filePollInterval).toInt)(
+    implicit transid: TransactionId): Future[Boolean] = {
+    wskc.isOomKilled(id)(TransactionId.invoker).flatMap { killed =>
+      if (killed) Future.successful(true)
+      else if (retries > 0) akka.pattern.after(filePollInterval, as.scheduler)(isOomKilled(retries - 1))
+      else Future.successful(false)
+    }
+  }
+
+  override protected def callContainer(path: String, body: JsObject, timeout: FiniteDuration, retry: Boolean = false)(
+    implicit transid: TransactionId): Future[RunResult] = {
+    val started = Instant.now()
+    val http = httpConnection.getOrElse {
+      val conn = if (Container.config.akkaClient) {
+        new AkkaContainerClient(addr.host, addr.port, timeout, ActivationEntityLimit.MAX_ACTIVATION_ENTITY_LIMIT, 1024)
+      } else {
+        new ApacheBlockingContainerClient(
+          s"${addr.host}:${addr.port}",
+          timeout,
+          ActivationEntityLimit.MAX_ACTIVATION_ENTITY_LIMIT)
+      }
+      httpConnection = Some(conn)
+      conn
+    }
+
+    http
+      .post(path, body, retry)
+      .flatMap { response =>
+        val finished = Instant.now()
+
+        response.left
+          .map {
+            // Only check for memory exhaustion if there was a
+            // terminal connection error.
+            case error: ConnectionError =>
+              isOomKilled().map {
+                case true  => MemoryExhausted()
+                case false => error
+              }
+            case other => Future.successful(other)
+          }
+          .fold(_.map(Left(_)), right => Future.successful(Right(right)))
+          .map(res => RunResult(Interval(started, finished), res))
+      }
+  }
 }
